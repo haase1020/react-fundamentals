d2e8804bee9cfa5300f3036d0ea9bec85a4d9273
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-12 07:50:37 -0400

    finish 07.js

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,51 +1,86 @@
 // Rendering Lists
-// http://localhost:3000/isolated/exercise/07.js
+// 💯 Focus Demo
+// http://localhost:3000/isolated/final/07.extra-1.js
 
 import React from 'react'
 
-const allItems = [
-  {id: 'a', value: 'apple'},
-  {id: 'o', value: 'orange'},
-  {id: 'g', value: 'grape'},
-  {id: 'p', value: 'pear'},
-]
+function FocusDemo() {
+  const [items, setItems] = React.useState([
+    {id: 'a', value: 'apple'},
+    {id: 'o', value: 'orange'},
+    {id: 'g', value: 'grape'},
+    {id: 'p', value: 'pear'},
+  ])
 
-function App() {
-  const [items, setItems] = React.useState(allItems)
+  React.useEffect(() => {
+    const id = setInterval(() => setItems(shuffle), 1000)
+    return () => clearInterval(id)
+  }, [])
 
-  function addItem() {
-    setItems([...items, allItems.find(i => !items.includes(i))])
-  }
-
-  function removeItem(item) {
-    setItems(items.filter(i => i !== item))
+  function getChangeHandler(item) {
+    return event => {
+      const newValue = event.target.value
+      setItems(allItems =>
+        allItems.map(i => ({
+          ...i,
+          value: i.id === item.id ? newValue : i.value,
+        })),
+      )
+    }
   }
 
   return (
-    <div
-      style={{
-        height: 200,
-        width: 400,
-        backgroundColor: '#eee',
-        borderRadius: 4,
-        padding: 20,
-      }}
-    >
-      <button disabled={items.length >= allItems.length} onClick={addItem}>
-        add item
-      </button>
-      <ul style={{listStyle: 'none', paddingLeft: 0}}>
+    <div>
+      <div>
+        <h1>Without a key</h1>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
-            <button onClick={() => removeItem(item)}>remove</button>{' '}
-            <label htmlFor={`${item.value}-input`}>{item.value}</label>{' '}
-            <input id={`${item.value}-input`} defaultValue={item.value} />
-          </li>
+          <input value={item.value} onChange={getChangeHandler(item)} />
         ))}
-      </ul>
+      </div>
+      <div>
+        <h1>With array index as key</h1>
+        {items.map((item, index) => (
+          <input
+            key={index}
+            value={item.value}
+            onChange={getChangeHandler(item)}
+          />
+        ))}
+      </div>
+      <div>
+        <h1>With a Proper Key</h1>
+        {items.map(item => (
+          <input
+            key={item.id}
+            value={item.value}
+            onChange={getChangeHandler(item)}
+          />
+        ))}
+      </div>
     </div>
   )
+}
+
+function shuffle(originalArray) {
+  const array = [...originalArray]
+  let currentIndex = array.length
+  let temporaryValue
+  let randomIndex
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
+
+function App() {
+  return <FocusDemo />
 }
 
 export default App
